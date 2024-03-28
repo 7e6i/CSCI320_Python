cd014a9ec8b2a9757aaf56ae54633a23766c1938
Spencer Anderson <sra6342@rit.edu>
2024-03-27 20:40:14 -0400

    fixed token order and cleaned up friend code

--- functionality.py
+++ functionality.py
@@ -86,12 +86,12 @@ def makeaccount(conn,curs, tokens):
 
 
 def login(conn, curs, tokens):
-    username = tokens[1]
-    password = tokens[2]
-
     if len(tokens)!=3:
         print('Invalid entry')
         return -1
+
+    username = tokens[1]
+    password = tokens[2]
 
     curs.execute("""SELECT user_id,username,password FROM p320_07."Reader";""")
     data = curs.fetchall()
@@ -104,33 +104,32 @@ def login(conn, curs, tokens):
         return -1
 
     user_id = users[username][0]
-    curs.execute("""UPDATE p320_07."Reader" SET last_access = %s WHERE user_id = %s;""",(datetime.datetime.now(),user_id))
+    curs.execute("""UPDATE p320_07."Reader" SET last_access = %s WHERE user_id = %s8;""",(datetime.datetime.now(),user_id))
     conn.commit()
     print("Logged in")
     return user_id
 
+
 def collection(conn, curs, tokens):
     pass
 
-def addfriend(conn, curs, passed_user_id, tokens):
-    friend_username = tokens[1]
-    friend_id = 0
-    friend_exist = False
 
-    if len(tokens)!=2:
+def addfriend(conn, curs, passed_user_id, tokens):
+    if len(tokens) != 2:
         print('Invalid entry')
         return
+
+    friend_username = tokens[1]
+    friend_id = -1
 
     curs.execute("""SELECT user_id, username FROM p320_07."Reader";""")
     reader_data = curs.fetchall()
     for reader in reader_data:
-        if friend_username == reader[1]:
-            friend_exist = True
-            friend_id = reader[0]
-    if not friend_exist:
-        print("there is no user with that username")
-        return
+        if friend_username == reader[1]: friend_id = reader[0]
 
+    if friend_id == -1:
+        print("There is no user with that username")
+        return
 
     curs.execute("""SELECT user_id, friend_id FROM p320_07."Friendship";""")
     friend_data = curs.fetchall()
@@ -138,7 +137,7 @@ def addfriend(conn, curs, passed_user_id, tokens):
     for friend in friend_data:
         if passed_user_id == friend[0] and friend_id == friend[1]:
             print('Already friends')
-            return -1
+            return
 
     curs.execute("""INSERT INTO p320_07."Friendship"
         (user_id, friend_id)
@@ -148,35 +147,41 @@ def addfriend(conn, curs, passed_user_id, tokens):
     conn.commit()
     print("Friend added")
 
-def removefriend(conn, curs, passed_user_id, tokens):
-    friend_username = tokens[1]
-    friend_id_input = 0;
-    friendship_exist = False;
 
-    if len(tokens)!=2:
+def removefriend(conn, curs, passed_user_id, tokens):
+    if len(tokens) != 2:
         print('Invalid entry')
         return
+
+    friend_username = tokens[1]
+    friend_id_input = -1
+    friendship_exist = False
 
     curs.execute("""SELECT user_id, username FROM p320_07."Reader";""")
     reader_data = curs.fetchall()
     for reader in reader_data:
-        if friend_username == reader[1]:
-            friend_id_input = reader[0]
+        if friend_username == reader[1]: friend_id_input = reader[0]
+
+    if friend_id_input == -1:
+        print("There is no user with that username")
+        return
 
     curs.execute("""SELECT user_id, friend_id FROM p320_07."Friendship";""")
     friend_data = curs.fetchall()
 
     for friend in friend_data:
         if passed_user_id == friend[0] and friend_id_input == friend[1]:
-            friendship_exist = True
 
-    if friendship_exist:
-        curs.execute("""DELETE FROM p320_07."Friendship"
-                        WHERE user_id=%s;""", (passed_user_id,))
-        print("Friendship was deleted")
-        conn.commit()
-    else:
-        print("No friend was found")
+            curs.execute("""DELETE FROM p320_07."Friendship" 
+                WHERE user_id=%s AND friend_id = %s;""",
+                (passed_user_id, friend_id_input))
+
+            print("Friendship was deleted")
+            conn.commit()
+
+            return
+
+    print("No friend was found")
 
 
 def test(conn, curs):
